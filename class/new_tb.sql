8342f61b376d3ca219ff55f59fc83c4d3e34fe23
Soren <danilo-258@hotmail.com>
2025-04-01 05:41:29 -0300

    chore: move query new past

--- class/new_tb.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE tb_alunos (
-  id_aluno int,
-  nome varchar(255) default NULL,
-  idade int default NULL,
-  interesse varchar(255) default NULL,
-  email varchar(255) default NULL,
-  estado varchar(255) default NULL
-);
