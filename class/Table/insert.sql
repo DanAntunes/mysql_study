961d1536ecc1a07bb571456ba0a68746050d3953
Soren <danilo-258@hotmail.com>
2025-04-02 03:31:20 -0300

    feat(database): inserir registros em tb_alunos
    - Adiciona múltiplos INSERTs para popular a tabela tb_alunos com dados de alunos.

---
+++ class/Table/insert.sql
@@ -0,0 +1,1 @@
+INSERT INTO tb_curso(ativo, carga_horaria,data_cadastro,id_curso, imagem_curso, investimento, nome_curso, resumo)values(1,35,'2025-07-17 12:35:25', 2, 'curso_angular.jpg', 575.86, 'web completo com JS, TS e Angular', 'Aprenda a criar aplicações front-end incriveis com JS, TS e Angular');
