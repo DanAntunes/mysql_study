6c1bc5037b405bd35b42d26db53d35e539cdac3c
Soren <danilo-258@hotmail.com>
2025-04-10 07:58:55 -0300

    feat(db): inserir descrições técnicas dos produtos
    - Adicionada descrição técnica do Inspiron Dell (produto 1) com design elegante e tela infinita.
    - Adicionada descrição técnica da smart TV Samsung (produto 2) com resolução Full HD.
    - Adicionada descrição técnica do smartphone LG (produto 3) destacando performance, design e recursos multimídia.

---
+++ loja_virtual/database/tables/insert-descricoes.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_descricoes_tecnicas(id_produto, descricao_tecnica) VALUES (1, 'O novo Inspiron Dell oferece um design elegante e tela infinita que amplia seus sentidos, mantendo a sofisticação e medidas compactas...');
+
+INSERT INTO tb_descricoes_tecnicas(id_produto, descricao_tecnica) VALUES (2, 'A smart TV da Samsung possui tela de 40" e oferece resolução Full HD, imagens duas vezes melhores que TVs HDs padrão...');
+
+INSERT INTO tb_descricoes_tecnicas(id_produto, descricao_tecnica) VALUES (3, 'Saia da mesmice. O smartphone LG está mais divertido, rápido, fácil, cheio de selfies e com tela HD de incríveis 5,3"...');
