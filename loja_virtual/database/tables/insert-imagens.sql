1918f5f43ee08ac26ed1beeb7cd73bafc2300f5d
Soren <danilo-258@hotmail.com>
2025-04-10 13:05:37 -0300

    feat(db): inserir imagens dos produtos
    - Inseridas imagens para o produto 1: notebook_1.jpg, notebook_2.jpg, notebook_3.jpg.
    - Inseridas imagens para o produto 2: smarttv_1.jpg, smarttv_2.jpg, smarttv_3.jpg.
    - Inseridas imagens para o produto 3: smartphone_1.jpg, smartphone_2.jpg, smartphone_3.jpg.

---
+++ loja_virtual/database/tables/insert-imagens.sql
@@ -0,0 +1,9 @@
+INSERT INTO tb_imagens(id_produto, url_imagem)
+VALUES(1, 'notebook_1.jpg'), (1, 'notebook_2.jpg'), (1, 'notebook_3.jpg');
+
+INSERT INTO tb_imagens(id_produto, url_imagem)
+VALUES(2, 'smarttv_1.jpg'), (2, 'smarttv_2.jpg'), (2, 'smarttv_3.jpg');
+
+INSERT INTO tb_imagens(id_produto, url_imagem)
+VALUES(3, 'smartphone_1.jpg'), (3, 'smartphone_2.jpg'), (3, 'smartphone_3.jpg');
+
