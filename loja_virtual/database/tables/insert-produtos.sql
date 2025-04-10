27458f3097a666b33a6c033c8c84e7c567044086
Soren <danilo-258@hotmail.com>
2025-04-10 07:59:31 -0300

    feat(produtos): inserir novos produtos na tabela
    - Inserido Notebook Dell Inspiron Ultrafino com Intel Core i7, 16GB RAM e 240GB SSD, valor R$3500.00.
    - Inserida Smart TV LED 40" Samsung Full HD com 2 HDMI, 1 USB e Wi-Fi integrado, valor R$1475.54.
    - Inserido Smartphone LG K10 Dual Chip com Android 7.0, 4G, Wi-Fi e câmera de 13MP, valor R$629.99.

---
+++ loja_virtual/database/tables/insert-produtos.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_produtos(produto, valor) VALUES ('Notebook Dell Inspiron Ultrafino Intel Core i7, 16GB RAM e 240GB SSD', 3500.00);
+
+INSERT INTO tb_produtos(produto, valor) VALUES ('Smart TV LED 40" Samsung Full HD 2 HDMI 1 USB Wi-Fi Integrado', 1475.54);
+
+INSERT INTO tb_produtos(produto, valor) VALUES ('Smartphone LG K10 Dual Chip Android 7.0 4G Wi-Fi Câmera de 13MP', 629.99);
